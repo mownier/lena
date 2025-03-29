109106220b9d6bd67024403c1d7dc7d0c5eafef4
Mounir Ybanez <rinuom91@gmail.com>
2025-03-29 14:40:45 +0800

    Added handling of environment variables LENA_PORT, LENA_ENDPOIONT, LENA_STORAGE.

--- main.go
+++ main.go
@@ -1,39 +1,126 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"lena/auth"
+	"lena/endpoints/grpcendpoint"
 	"lena/endpoints/httpendpoint"
-	"lena/server"
+	"lena/storages"
 	"lena/storages/inmemorystorage"
 	"log"
 	"net"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
+
+	"google.golang.org/grpc"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
-		log.Fatalf("Error getting network interfaces: %v", err)
+		log.Fatalln("Error getting network interfaces:", err)
 	}
+	localIP := ""
 	for _, addr := range addrs {
 		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
 			if ipnet.IP.To4() != nil {
-				localIP := ipnet.IP.String()
-				fmt.Printf("lena listening on: http://%s:%s\n", localIP, port)
+				localIP = ipnet.IP.String()
 			}
 		}
 	}
-	storage := inmemorystorage.NewInMemoryStorage()
-	authServer := server.NewAuthServer(storage, storage)
+	port, err := getPort()
+	if err != nil {
+		log.Fatalln("Error getting port:", err)
+	}
+	storage, err := getStorage()
+	if err != nil {
+		log.Fatalln("Error getting storage:", err)
+	}
+	endpoint, err := getEndpoint()
+	if err != nil {
+		log.Fatalln("Error getting storage:", err)
+	}
+	var userStorage storages.UserStorage
+	var sessionStorage storages.SessionStorage
+	if storage == "inmemory" {
+		inmemorystorage := inmemorystorage.NewInMemoryStorage()
+		userStorage = inmemorystorage
+		sessionStorage = inmemorystorage
+	}
+	authServer := auth.NewServer(userStorage, sessionStorage)
+	if endpoint == "http" {
+		runHTTPEndpoint(localIP, port, authServer)
+		return
+	}
+	if endpoint == "grpc" {
+		runGRPCEndpoint(localIP, port, authServer)
+		return
+	}
+}
+
+func getPort() (int, error) {
+	input := os.Getenv("LENA_PORT")
+	if input == "" {
+		return 8080, nil
+	}
+	port, err := strconv.Atoi(input)
+	if err != nil {
+		return -1, err
+	}
+	return port, nil
+}
+
+func getStorage() (string, error) {
+	array := []string{"inmemory", "sqlite"}
+	input := os.Getenv("LENA_STORAGE")
+	if input == "" {
+		return array[0], nil
+	}
+	input = strings.ToLower(input)
+	for _, value := range array {
+		if input == value {
+			return input, nil
+		}
+	}
+	return "", errors.New("selected storage option not found")
+}
+
+func getEndpoint() (string, error) {
+	array := []string{"http", "grpc"}
+	input := os.Getenv("LENA_ENDPOINT")
+	if input == "" {
+		return array[0], nil
+	}
+	input = strings.ToLower(input)
+	for _, value := range array {
+		if input == value {
+			return input, nil
+		}
+	}
+	return "", errors.New("selected endpoint option not found")
+}
+
+func runHTTPEndpoint(localIP string, port int, authServer *auth.Server) {
 	mux := http.NewServeMux()
 	httpendpoint.SetupHTTPHandlers(mux, authServer)
-	err = http.ListenAndServe(":"+port, mux)
+	fmt.Printf("lena HTTP server listening on: http://%s:%d\n", localIP, port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux); err != nil {
+		log.Fatalln("failed to serve:", err)
+	}
+}
+
+func runGRPCEndpoint(localIP string, port int, authServer *auth.Server) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
-		log.Fatalln("Error startng server:", err)
+		log.Fatalln("failed to listen:", err)
+	}
+	grpcServer := grpc.NewServer()
+	grpcendpoint.RegisterLenaServiceServer(grpcServer, grpcendpoint.NewServer(authServer))
+	fmt.Printf("lena GRPC server listening on: tcp://%s:%d\n", localIP, port)
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalln("failed to server:", err)
 	}
 }
